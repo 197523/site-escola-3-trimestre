a536756388f455ff82fac584385e29ad6b195653
Lucas Bilecki <105056272+197523@users.noreply.github.com>
2024-09-17 10:49:50 -0300

    fimose 2

--- perguntas.js
+++ perguntas.js
@@ -1,99 +1,99 @@
 criaCartao(
     'Matemática',
     'Qual a fórmula de Bhaskara?',
-    'x=(-b±√b²-4.a.b)/2.a').
+    'x=(-b±√b²-4.a.b)/2.a')
     
     criaCartao(
     'Matemática',
     'Qual o teorema de Pitágoras?',
-    'a²=b²+c²').
+    'a²=b²+c²')
     
     criaCartao(
     'Matemática',
     'Como calcular o termo geral de uma PA?',
-    'an=a¹+(n-1).r').
+    'an=a¹+(n-1).r')
     
     criaCartao(
     'Matemática',
     'Como calcular a soma dos termos de uma PA?',
-    'Sn=(n.(a¹+an))/2').
+    'Sn=(n.(a¹+an))/2')
     
     criaCartao(
     'Matemática',
     'Como calcular o termo geral de uma PG?',
-    'an=a1.qn-¹').
+    'an=a1.qn-¹')
     
     criaCartao(
     'Matemática',
     'Como calcular a soma dos termos de uma PG Finita?',
-    'Sn=(a1.(Qn-1))/Q-1').
+    'Sn=(a1.(Qn-1))/Q-1')
     
     criaCartao(
     'Física',
     'Qual a fórmula da velocidade',
-    't/m').
+    't/m')
     
     criaCartao(
     'Física',
     'Qual a fórmula da enegia',
-    'E=mc²').
+    'E=mc²')
     
     criaCartao(
     'Física',
     'Qual a medida correspondente a aceleração da gravidade da Terra',
-    '9,8m/s').
-    
+    '9,8m/s')
+
     criaCartao(
     'Física',
     'Qual a primeira lei de Newton',
-    'Princípio da Inércia').
+    'Princípio da Inércia')
     
     criaCartao(
     'Física',
     'Qual a segunda lei de Newton',
-    'Lei da superposição de forças').
+    'Lei da superposição de forças')
     
     criaCartao(
     'Física',
     'Qual a terceira lei de Newton',
-    'Princípio da ação e reação').
+    'Princípio da ação e reação')
     
     criaCartao(
     'História',
     'Quem foi o primeiro imperador de Roma',
-    'Rómulo Augustolo').
+    'Rómulo Augustolo')
     
     criaCartao(
     'História',
     'Qual o primeiro imperador de Roma de que temos registro',
-    'Augusto Cesar').
+    'Augusto Cesar')
     
     criaCartao(
     'História',
     'Quando morreu Getúlio Vargas',
-    '24 de agosto de 1954).'
+    '24 de agosto de 1954)')
     
     criaCartao(
     'História',
     'Quando foi a Proclamação da República',
-    '15 de novembro de 1889').
+    '15 de novembro de 1889')
     
     criaCartao(
     'Química',
     'Como identificar um ácido',
-    'Presença de hidrogênio pressupondo outro elemento ').
+    'Presença de hidrogênio pressupondo outro elemento ')
     
     criaCartao(
     'Química',
     'Como identificar uma base',
-    'Presença de hidroxila OH').
+    'Presença de hidroxila OH')
     
     criaCartao(
     'Química',
     'Quantas ligações faz o carbono',
-    '6').
+    '6')
     
     criaCartao(
     'Química',
     'Quantas ligações faz o hidrogênio',
-    '1').
+    '1')
